e3bcc4fbd07aa6a04c0977405570f1ea41245c8e
Rich Greenhill <rgnet1@yahoo.com>
2016-04-07 19:26:10 -0700

    create Books reducer and reference from reducer/index

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Javascript: The Good Partt" },
+    { title: "Eloquent Ruby" },
+    { title: "Harry Potter and the Goblet of Fire" },
+    { title: "The Stand" }
+  ];
+}

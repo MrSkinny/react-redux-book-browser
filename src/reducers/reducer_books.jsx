ed629a86bdede9c977b7cac76da7884d7e456c2c
Rich Greenhill <rgnet1@yahoo.com>
2016-04-11 05:13:34 -0700

    add additional reducer data, pass to view

--- src/reducers/reducer_books.jsx
+++ src/reducers/reducer_books.jsx
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: "Javascript: The Good Partt" },
-    { title: "Eloquent Ruby" },
-    { title: "Harry Potter and the Goblet of Fire" },
-    { title: "The Stand" }
+    { title: "Javascript: The Good Parts", pages: 74 },
+    { title: "Eloquent Ruby", pages: 39 },
+    { title: "Harry Potter and the Goblet of Fire", pages: 103 },
+    { title: "The Stand", pages: 403 }
   ];
 }

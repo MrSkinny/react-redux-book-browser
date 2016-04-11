fa30ff6869a24afb85b1055648c54a111e35ef79
Rich Greenhill <rgnet1@yahoo.com>
2016-04-11 05:00:11 -0700

    promoted BookDetail to container

--- src/containers/book_detail.jsx
+++ src/containers/book_detail.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
   render() {
     return(
       <div>
@@ -9,3 +10,12 @@ export default class BookDetail extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  // This object shows up as props in BookDetail
+  return {
+    book: state.activeBook
+  };
+}
+
+export default connect(mapStateToProps)(BookDetail);
